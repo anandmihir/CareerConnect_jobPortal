122d6674853408b56b549bdcacd1cc6d2c980187
Mihir Anand <anandmihir77@gmail.com>
2025-04-17 11:00:39 +0530

    mihir

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                     <img
                         src={assets.CareerConnect}
                         alt="CareerConnect Logo"
-                        className="w-20 h-20 sm:w-224 sm:h-20"
+                        className="w-30 h-30 sm:w-36 sm:h-20"
                     />
                 </div>
 
