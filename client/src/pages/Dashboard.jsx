1efa822a1e4cec948ade6959f951390da3c29a04
Mihir Anand <anandmihir77@gmail.com>
2025-04-20 12:35:23 +0530

    mihir

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -26,22 +26,27 @@ const Dashboard = () => {
     return (
         <div className='min-h-screen'>
 
-            {/* Navbar for Recuriter Panel */}
-            <div className='shadow py-4'>
-                <div className='px-5 flex justify-between items-center'>
-                    <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.CareerConnect} alt="" />
-                    {companyData && (
-                        <div className='flex items-center gap-3'>
-                            <p className='max-sm:hidden'>Welcome, {companyData.name}</p>
-                            <div className='relative group'>
-                                <img className='w-8 border rounded-full' src={companyData.image} alt="" />
-                                <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded  pt-12'>
-                                    <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                                        <li onClick={logout} className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+           {/* Navbar for Recruiter Panel */}
+<div className='shadow py-4'>
+    <div className='px-5 flex justify-between items-center'>
+        <img
+            onClick={e => navigate('/')}
+            src={assets.CareerConnect}
+            alt="CareerConnect Logo"
+            className="w-60 h-60 sm:w-66 sm:h-20 cursor-pointer"
+        />
+        {companyData && (
+            <div className='flex items-center gap-3'>
+                <p className='max-sm:hidden'>Welcome, {companyData.name}</p>
+                <div className='relative group'>
+                    <img className='w-8 border rounded-full' src={companyData.image} alt="" />
+                    <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                        <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                            <li onClick={logout} className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
                     )}
                 </div>
             </div>
